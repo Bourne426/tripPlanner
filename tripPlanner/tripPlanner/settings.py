d048577f840ff0f36b95dfaa42ff91374bb0e8f6
rahul <rahulkumar.s17@iiits.in>
2019-04-13 21:36:57 +0530

    Gui added

--- tripPlanner/tripPlanner/settings.py
+++ tripPlanner/tripPlanner/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 'package',
-    'django_extensions',
+    # 'django_extensions',
     'account',
     'widget_tweaks',
     'upload_package',
